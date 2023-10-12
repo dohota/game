077680ca7b236007ae92756a030dce75ed597661
dohota <103027061+dohota@users.noreply.github.com>
2023-10-12 20:27:41 +0800

    Add files via upload

---
+++ 元素卡牌游戏/tktk2.0.py
@@ -0,0 +1,152 @@
+import threading
+import tkinter
+from tkinter import messagebox
+import random
+import time
+import sys
+#
+# 准备放弃tk了，因为太难搞，资料也不多
+# 准备用别的gui，反正逻辑也差不多！
+#
+#
+#
+#
+#
+
+
+class Game:
+    energy1 = energy2 = 0
+    defend_time1 = defend_time2 = 3
+    round = 1
+    a = -1
+    enemy_action = -1
+
+    @staticmethod
+    def showing(a, b):
+        messagebox.showinfo(a, b)
+
+    @staticmethod
+    def energy():
+        Game.energy1 += 1
+
+    @staticmethod
+    def defend():
+        if Game.defend_time1 <= 0:
+            Game.showing("Warn!", "not allowed")
+            return
+        Game.defend_time1 -= 1
+
+    @staticmethod
+    def attack():
+        if Game.energy1 <= 0:
+            Game.showing("Warn!", "lack energy")
+            return
+        Game.energy1 -= 1
+
+    def loop(self, s):
+        print("adsdsdsdsdsdsd")
+        if Game.enemy_action == 0 and Game.a == 2:
+            Game.showing("you lose", "What a pity!")
+            s.master.quit()
+        if Game.enemy_action == 2 and Game.a == 0:
+            Game.showing("you win", "Good job!")
+            s.master.quit()
+        if Game.a != -1:
+            p1 = tkinter.Label(s.master, text="Your turn:"+["energy", "defend", "attack"][Game.a])
+            p1.pack()
+            p2 = tkinter.Label(s.master, text="Enemy turn:"+["energy", "defend", "attack"][Game.enemy_action])
+            p2.pack()
+            time.sleep(1)
+            s.clean_screen()
+        s.master.after(350, self.loop(s))
+
+
+class Window(tkinter.Frame):
+
+    def __init__(self, master):
+        super().__init__(master)
+        self.master = master
+        self.master.title("Fighting Game")
+        self.master.geometry("500x200")
+        # 绑定关闭窗口的方法
+        self.master.bind("<Destroy>", self.on_window_close)
+        #self.v = self.master.StringVar()
+        self.aa = ["energy", "defend", "attack"]
+        self.pack()
+        # 删除旧的部件
+        while self.winfo_children():
+            self.winfo_children()[0].destroy()
+        self.create()
+
+    def on_window_close(self,event):
+        # event这个参数不能丢
+        # 如果选择关闭窗口 ，不管是哪一个窗口，都会结束所有线程，结束整个程序
+        print("close the window")
+        self.master.quit()
+        del self
+        sys.exit()
+
+    def create(self):
+        if Game.energy2 == 0:
+            Game.enemy_action = int(random.random()*1000) % 2
+        else:
+            Game.enemy_action = int(random.random() * 1000) % 3
+        if Game.defend_time2 == 0:
+            Game.enemy_action = 0
+        if Game.enemy_action == 0:
+            Game.energy2 += 1
+        elif Game.enemy_action == 1:
+            Game.defend_time2 -= 1
+        elif Game.enemy_action == 2:
+            Game.energy2 -= 1
+        l1 = tkinter.Label(self.master, text="Round"+str(Game.round))
+        l1.pack()
+        # 把Radiobutton放置在frame之中
+        frame = tkinter.Frame(self.master)
+        frame.pack()
+        self.v = tkinter.StringVar()
+        self.v.set("energy")  # 默认值
+        r1 = tkinter.Radiobutton(self.master, text="Energy", value="energy", variable=self.v)
+        # 不要写variable= tkinter.StringVar()，因为那样会导致Radiobutton选项不互斥
+        r1.pack()
+        r2 = tkinter.Radiobutton(self.master, text="Defend", value="defend", variable=self.v)
+        r2.pack()
+        r3 = tkinter.Radiobutton(self.master, text="Attack", value="attack", variable=self.v)
+        r3.pack()
+        b1 = tkinter.Button(self.master, text="Start the round", command=self.check_option())
+        b1.pack()
+
+    def check_option(self):
+        if self.v.get() == "energy":
+            Game.a = 0
+            Game.energy()
+        elif self.v.get() == "defend":
+            Game.a = 1
+            Game.defend()
+        elif self.v.get() == "attack":
+            Game.a = 2
+            Game.attack()
+
+    def clean_screen(self):
+        # 游戏进入下一回合
+        Game.round += 1
+        # 重置这些参数，给下一回合使用
+        Game.enemy_action = -1
+        # 删除当前的Frame (清空屏幕)
+        while self.winfo_children():
+            self.winfo_children()[0].destroy()
+        # 创建并添加一个新的Frame来替换旧的Frame
+        new_frame = tkinter.Frame(self)
+        new_frame.pack(fill="both", expand=True)
+
+
+if __name__ == "__main__":
+    # 设置最大递归深度
+    sys.setrecursionlimit(900)
+    # master是其父窗口
+    app = Window(master=tkinter.Tk())
+    # 创建线程并启动
+    g = Game()
+    app.mainloop()
+    thread = threading.Thread(target=g.loop(app))
+    thread.start()
